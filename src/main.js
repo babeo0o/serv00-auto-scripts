fdd1d5d23385ab6cf80daab7ed095a0af2f7a82d
babeo0o <sy503445884@gmail.com>
2024-12-22 22:13:35 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -58,7 +58,7 @@ async function sendTelegramMessage(token, chatId, message) {
         });
         const page = await browser.newPage();
         // await page.setViewport({ width: 1366, height: 768 });
-        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36');
+        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36');
         // await page.evaluateOnNewDocument(() => {
         //     delete Object.getPrototypeOf(navigator).webdriver;
         // });
